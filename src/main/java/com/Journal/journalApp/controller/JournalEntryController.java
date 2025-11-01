37c7882b717cb76d8daf44a5cf9fdc31cd951e60
Aryan <singharyan09@gmail.com>
2025-11-01 15:29:42 +0530

    database

--- src/main/java/com/Journal/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/Journal/journalApp/controller/JournalEntryController.java
@@ -1,51 +1,93 @@
 package com.Journal.journalApp.controller;
 
 
-import com.Journal.journalApp.JournalService.Journalserviceimp;
+//import com.Journal.journalApp.JournalService.Journalserviceimp;
 import com.Journal.journalApp.JournalService.journalService;
 import com.Journal.journalApp.entity.JournalEntry;
 import com.Journal.journalApp.exception.JournalNotfoundException;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
 @RequestMapping("/Journal")
+@CrossOrigin
 public class JournalEntryController {
 
     @Autowired
-    journalService journalService;
+    private journalService journalService;
+
+    @PostMapping
+    public ResponseEntity<JournalEntry> saveEntry(@RequestBody JournalEntry journalEntry){
+        try {
+            journalEntry.setDate(LocalDateTime.now());
+            journalService.saveEntry(journalEntry);
+            return new ResponseEntity<>(journalEntry, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+
+    }
 
     @GetMapping
     public List<JournalEntry> getAll(){
-        return new ArrayList<JournalEntry>(journalService.getJournal());
+        return journalService.findAll();
     }
 
-    @PostMapping
-    public boolean createEntry(@RequestBody JournalEntry journalEntry){
-        journalService.createJournal(journalEntry);
+
+    @GetMapping("id/{myId}")
+    public ResponseEntity<JournalEntry> getById(@PathVariable ObjectId myId){
+        Optional<JournalEntry> journalEntry = journalService.findById(myId);
+        return journalEntry.map(entry -> new ResponseEntity<>(entry, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+    }
+
+    @DeleteMapping("id/{myId}")
+    public boolean deleteById(@PathVariable ObjectId myId){
+        journalService.delete(myId);
         return true;
     }
 
-
-    @PutMapping("{id}")
-    public String updateEntry(@PathVariable("id") long id  , @RequestBody JournalEntry journalEntry){
-        if(journalService.existsById(id)){
-            journalService.updateJournal(id,journalEntry);
-            return  "Journal Updated";
+    @PutMapping("/id/{id}")
+    public ResponseEntity<JournalEntry> updateEntry(@PathVariable ObjectId id, @RequestBody JournalEntry newEntry){
+        JournalEntry old = journalService.findById(id).orElse(null);
+        if(old != null){
+            old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().isEmpty() ? newEntry.getTitle() : old.getTitle());
+            old.setContent(newEntry.getContent() != null && !newEntry.getContent().isEmpty() ? newEntry.getContent() : old.getContent());
+            return new ResponseEntity<>(old, HttpStatus.OK);
         }
-        else throw new JournalNotfoundException();
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping
-    public boolean deleteEntry(@RequestBody JournalEntry journalEntry){
-        journalService.deleteJournal(journalEntry.getId());
-        return false;
-    }
+//    @PutMapping("{id}")
+//    public String updateEntry(@PathVariable("id") String id  , @RequestBody JournalEntry journalEntry){
+//        if(journalService.existsById(id)){
+//            journalService.updateJournal(id,journalEntry);
+//            return  "Journal Updated";
+//        }
+//        else throw new JournalNotfoundException();
+//    }
+//
+//    @DeleteMapping
+//    public boolean deleteEntry(@RequestBody JournalEntry journalEntry){
+//        journalService.deleteJournal(journalEntry.getId());
+//        return false;
+//    }
 
 
 }
+
+//controller -- > service -- > repository
+
+
+// for regenerated instance
+//    @GetMapping
+//    public List<JournalEntry> getAll(){
+//        return new ArrayList<JournalEntry>(journalService.getJournal());
+//    }
--- src/main/java/com/Journal/journalApp/entity/JournalEntry.java
+++ src/main/java/com/Journal/journalApp/entity/JournalEntry.java
@@ -1,19 +1,29 @@
 package com.Journal.journalApp.entity;
 
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document()
 public class JournalEntry {
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
 
-    private long id ;
+    @Id
+    private ObjectId id ;
     private String title;
     private String content;
+    private LocalDateTime date;
 
+
+    public ObjectId getId() { return id; }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
@@ -30,4 +40,30 @@ public class JournalEntry {
     public void setContent(String content) {
         this.content = content;
     }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+
+
+
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+
+
+
+
+
+
+
 }
